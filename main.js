55c805bd08baf2df27c7daf577448dd8abbf96c2
Francesco Maestrelli <framae26@gmail.com>
2022-01-24 16:38:43 +0100

    buttons

--- main.js
+++ main.js
@@ -8,20 +8,22 @@
 
 // // Dati e sconti legati all'età
 
-// if (age < 18){
-// result = 0.21 * 0.8 * distance;
-// } 
 
-// else if (age > 65){
-// result = 0.21 * 0.6 * distance;
-// }
-
-// else {
-// result = 0.21 * distance;
-// }
 
 // document.getElementById("result").innerHTML=result.toFixed(2);
 
 let name = document.getElementById('name');
 let number = document.getElementById('number');
 let age = document.getElementById('age');
+
+//  if (age < 18){
+//  result = 0.21 * 0.8 * distance;
+// } 
+
+//  else if (age > 65){
+//  result = 0.21 * 0.6 * distance;
+// }
+
+//  else {
+//  result = 0.21 * distance;
+// }
