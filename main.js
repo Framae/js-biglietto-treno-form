f3006a1f3278432313b00f4c87d66770e301cea8
Francesco Maestrelli <framae26@gmail.com>
2022-01-24 15:39:51 +0100

    input

---
+++ main.js
@@ -0,0 +1,24 @@
+// //Distanza da percorrere
+
+// let distance = parseInt(prompt("Quanti Km devi percorrere?"));
+
+// //Età del cliente
+
+// let age = parseInt(prompt("Inserisci la tua età"));
+
+// // Dati e sconti legati all'età
+
+// if (age < 18){
+// result = 0.21 * 0.8 * distance;
+// } 
+
+// else if (age > 65){
+// result = 0.21 * 0.6 * distance;
+// }
+
+// else {
+// result = 0.21 * distance;
+// }
+
+// document.getElementById("result").innerHTML=result.toFixed(2);
+
